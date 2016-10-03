81efbbcc2fa822b6d26bcbc7e38502d3f49a2cca
john8222 <john82mini@gmail.com>
2016-10-03 21:51:52 +0530

    Add files via upload

---
+++ mii.r
@@ -0,0 +1,24 @@
+ma<- matrix (1:12,3,4)
+nrow(ma) #3
+ncolumn(ma)  #4
+ncol(ma)
+ncol(array(1:24, dim=2:4))  #3the second dimension
+NCOL(1:12)   #1
+NROW(1:12)   #12
+boot::
+  Matrix::
+  
+  
+  
+  c.noquote()
+
+
+
+
+
+
+grid(
+  
+  
+)
+acf()
